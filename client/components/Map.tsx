f968cf4b6515c661d26ea18ad36f7ffd6aba169b
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-11-14 19:58:10 +1300

    adding new map layer

--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -7,9 +7,9 @@ export default function Map() {
   useEffect(() => {
     const map = L.map('map').setView([-39.2806, 176.9120], 8);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    
     }).addTo(map)
   
   greatWalks.forEach((walk: GreatWalk) => {
