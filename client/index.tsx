49aaf033f0bd6adccc69350cd95e595dfe778088
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-11-16 19:01:35 +1300

    putting client key away

--- client/index.tsx
+++ client/index.tsx
@@ -3,16 +3,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
+import dotenv from 'dotenv'
 
+dotenv.config()
 import router from './router.tsx'
-
+const clientId = process.env.AUTH0_CLIENT_ID!
 const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <Auth0Provider
     domain="manaia2024-lucy.au.auth0.com"
-    clientId="pgiYPucNSTPpNMGZXqgbaDvs7vLXqOGL"
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin,
       audience: 'https://tracks/api',
