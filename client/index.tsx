f04bdc9c6c3dedd73a97680297d83ced53b57a36
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-11-16 19:32:41 +1300

    changing protcol for key access

--- client/index.tsx
+++ client/index.tsx
@@ -3,11 +3,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
-import dotenv from 'dotenv'
 
-dotenv.config()
 import router from './router.tsx'
-const clientId = process.env.AUTH0_CLIENT_ID!
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
 const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
