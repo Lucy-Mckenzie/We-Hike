575d959f43f5ca800277d115b1f6fc1cee11f0f5
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-11-18 11:11:54 +1300

    adding markers css

--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -5,6 +5,7 @@ import {
   Marker,
   Popup,
   TileLayer,
+  LayersControl
 } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 
@@ -22,7 +23,7 @@ export default function Map() {
       url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
       attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     />
-    
+    <LayersControl position="topright">
     {greatWalks.map((walk: GreatWalk) => (
       <Marker key={walk.name} position={walk.location as [number, number]} title={`marker for ${walk.name}`}>
         <Popup>
@@ -33,6 +34,7 @@ export default function Map() {
         </Popup>
       </Marker>
     ))}
+    </LayersControl>
   </MapContainer>
  )
 }
