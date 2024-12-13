3b569c74acfafd0455e776a928c98a64e8e8522c
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-13 14:59:40 +1300

    adding hook for hut details

--- client/pages/HutsInRegion.tsx
+++ client/pages/HutsInRegion.tsx
@@ -1,21 +1,19 @@
 import { useParams } from 'react-router'
-import data from '../data/data.json'
 
 export default function HutsInRegion() {
 
   const { region } = useParams()
 
-  const hutsInRegion = data.huts.filter((huts) => huts.region === region) 
   
   if (!region) return 'Sorry region does not exist'
-  if (hutsInRegion.length === 0) return 'sorry could not find huts'
-  
+  if (isPending)  return <>Loading...</>
+  if (error) return <>Sorry, cannot find hut details.</>
 
   return (
     <div className="flex flex-col items-start px-5 py-10 text-left mx-auto max-w-[900px]">
       <h1 className="text-4xl text-left mb-5 font-light">Huts in the {region} Region</h1>
       <ul>
-        {hutsInRegion.map((hut) => (
+        {huts.map((hut) => (
           <li key={hut.assetId}>
             {hut.name} - {hut.status}
           </li>
