142581d65172c4e76751d0db9db1e82e41468a1e
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-20 17:32:57 +1300

    adjusting photo

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -3,15 +3,15 @@ import Greatwalks from '../components/GreatWalks'
 
 export default function Home() {
   return (
-    <>
-    <img src="/images/roys.jpg" alt="roys peak" className="h-[600px] w-full"></img>
-    <div className="mx-auto flex max-w-[900px] flex-col items-start px-5 py-10 text-left">
+    <div className="flex max-w-[900px] flex-col justify-center items-center mx-auto">
+        <img src="/images/roys.jpg" alt="roys peak" className="h-auto w-auto items-center justify-center rounded-sm"></img>
+    <div className="px-5 py-10 text-left items-center">
       <h1 className="font-lato mb-5 text-left text-4xl font-light">
         The great walks of New Zealand, mapped
       </h1>
       <Map />
       <Greatwalks />
     </div>
-    </>
+    </div>
   )
 }
