7d7080b5252dadaefc17aadee8ca78ffa2ee7094
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-20 17:45:32 +1300

    changing logo for accessability

--- client/components/layout/NavBar.tsx
+++ client/components/layout/NavBar.tsx
@@ -12,22 +12,22 @@ export default function Nav() {
     <nav className="relative">
       <div className="bg-[##1C1C1C] flex items-center shadow-[3px_6px_10px_rgba(0,_0,_0,_0.04)] px-6 py-2 justify-between z-2">
       <NavLink to="/">
-      <img src="/images/logo.png" alt="Logo of we hike" className="h-[50px] ml-20 mr-auto z-1"/>
+      <img src="/images/logo.png" alt="Logo of we hike" className="h-[40px] ml-20 mr-auto z-1 hover:scale-105 transition-transform duration-200 hover:cursor-pointer"/>
       </NavLink>
       <div className="px-9 text-black flex items-center no-underline">
-      <NavLink className="ml-auto mr-11 flex text-black" to="/">Home</NavLink>
-      <NavLink className="ml-auto mr-11 flex text-black" to="/huts">Huts</NavLink>
-      <NavLink className="ml-auto mr-11 flex text-black" to="/tracks/region">Hikes</NavLink>
-      <NavLink className="ml-auto mr-11 flex text-black" to="/about">About</NavLink>
-      <NavLink className="ml-auto mr-11 flex text-black" to="/reviews">Reviews</NavLink>
+      <NavLink className="ml-auto mr-11 flex text-black hover:text-gray-400 hover:scale-105 transition-transform duration-200 hover:cursor-pointer" to="/">Home</NavLink>
+      <NavLink className="ml-auto mr-11 flex text-black hover:text-gray-400 hover:scale-105 transition-transform duration-200 hover:cursor-pointer" to="/huts">Huts</NavLink>
+      <NavLink className="ml-auto mr-11 flex text-black hover:text-gray-400 hover:scale-105 transition-transform duration-200 hover:cursor-pointer" to="/tracks/region">Hikes</NavLink>
+      <NavLink className="ml-auto mr-11 flex text-black hover:text-gray-400 hover:scale-105 transition-transform duration-200 hover:cursor-pointer" to="/about">About</NavLink>
+      <NavLink className="ml-auto mr-11 flex text-black hover:text-gray-400 hover:scale-105 transition-transform duration-200 hover:cursor-pointer" to="/reviews">Reviews</NavLink>
           <IfAuthenticated>
           <div className="flex flex-col items-center space-y-2">
-            <button className=" text-black text-sm mr-2 hover:sm-black-500" onClick={handleSignOut}>Sign out</button>
+            <button className=" text-black text-sm mr-2 hover:text-gray-400 hover:scale-105 transition-transform duration-200 hover:cursor-pointer" onClick={handleSignOut}>Sign out</button>
             {user && <p className="text-sm text-black">User: {user?.nickname}</p>}
           </div>
           </IfAuthenticated>
           <IfNotAuthenticated>
-            <button  className="border-lg rounded-lg bg-[#4A5D5E] text-sm px-2 py-1 text-white" onClick={handleSignIn}>Sign in</button>
+            <button  className="border-lg rounded-lg bg-[#4A5D5E] text-sm px-2 py-1 text-white hover:scale-105 transition-transform duration-200 hover:cursor-pointer" onClick={handleSignIn}>Sign in</button>
           </IfNotAuthenticated>
       </div>
       </div>
--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -4,7 +4,7 @@ import Greatwalks from '../components/GreatWalks'
 export default function Home() {
   return (
     <div className="flex max-w-[900px] flex-col justify-center items-center mx-auto">
-        <img src="/images/roys.jpg" alt="roys peak" className="h-auto w-auto items-center justify-center rounded-sm"></img>
+        <img src="/images/roys.jpg" alt="roys peak" className="h-auto w-auto items-center justify-center rounded-sm shadow-xl"></img>
     <div className="px-5 py-10 text-left items-center">
       <h1 className="font-lato mb-5 text-left text-4xl font-light">
         The great walks of New Zealand, mapped
