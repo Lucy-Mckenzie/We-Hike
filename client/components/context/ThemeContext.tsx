9b17908d060013e1461c87f9b4c73a2994da1200
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-01 18:42:52 +1300

    Adding daisyui, responsive navbar and toggle

---
+++ client/components/context/ThemeContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect, ReactNode } from 'react';
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+interface ThemeContextType {
+  theme: string;
+  changedTheme: (theme: string) => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+
+export default function ThemeProvider({children}: ThemeProviderProps) {
+  const [theme, setTheme] = useState('light')
+  const [mounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+    const storedTheme = localStorage.getItem('theme') || 'light'
+    setTheme(storedTheme)
+  }, [])
+
+  if (!mounted) return <p>Loading....</p>
+
+  const changedTheme = (theme: string) => {
+    setTheme(theme)
+    localStorage.setItem('theme', theme)
+  }
+
+  return (
+    <ThemeContext.Provider value={{theme, changedTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
