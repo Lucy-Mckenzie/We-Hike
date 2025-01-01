98d66b29b66c49a2084c46e834810bd5d16f0c25
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-01 17:14:12 +1300

    adding context for day and night

--- client/components/layout/Layout.tsx
+++ client/components/layout/Layout.tsx
@@ -1,18 +1,22 @@
 import { Outlet } from 'react-router-dom'
-
+import ClientThemeWrapper from './context/ClientThemeWrapper'
+import ThemeProvider from './context/ThemeContext'
+import './globals.css'
 import Footer from './Footer.tsx'
 import Nav from './NavBar.tsx'
 
 export default function Layout() {
   return (
-    <>
       <div>
+      <ThemeProvider>
+      <ClientThemeWrapper>
         <Nav />
         <div>
           <Outlet />
         </div>
         <Footer />
       </div>
-    </>
+      </ClientThemeWrapper>
+      </ThemeProvider>
   )
 }
