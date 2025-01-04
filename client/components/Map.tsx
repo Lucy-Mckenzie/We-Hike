5fe2bf794274c39cde27862f1f37e92c477d7d77
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-04 18:49:42 +1300

    organising main home

--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -5,7 +5,6 @@ import {
   Marker,
   Popup,
   TileLayer,
-  LayersControl
 } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
@@ -18,28 +17,28 @@ const markerIcon = new L.Icon({
 export default function Map() {
 
  return (
-  <MapContainer
-    className='w-full h-[50vh] md:h-[70vh] lg:h-[500px]'
-    center={[-39.2806, 176.9120]}
-    zoom={8}
-    aria-label='Map displaying the Great Walks of New Zealand'
-  >
-    <TileLayer
-      url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-      attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    />
-    <LayersControl position='topright'>
-    {greatWalks.map((walk: GreatWalk) => (
-      <Marker key={walk.name} position={walk.location as [number, number]} title={`marker for ${walk.name}`} icon={markerIcon}>
-        <Popup>
-          <b>{walk.name}</b><br />
-          Region: {walk.region}<br />
-          Description: {walk.descriptionS}<br />
-          Distance: {walk.distance} km
-        </Popup>
-      </Marker>
-    ))}
-    </LayersControl>
-  </MapContainer>
+  <div className='w-full h-[50vh] md:h-[70vh] lg:h-[500px] rounded-md overflow-hidden'>
+      <MapContainer
+        className='w-full h-full'
+        center={[-39.2806, 176.9120]}
+        zoom={8}
+        aria-label='Map displaying the Great Walks of New Zealand'
+      >
+        <TileLayer
+          url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+          attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        />
+        {greatWalks.map((walk: GreatWalk) => (
+          <Marker key={walk.name} position={walk.location as [number, number]} title={`marker for ${walk.name}`} icon={markerIcon}>
+            <Popup>
+              <b>{walk.name}</b><br />
+              Region: {walk.region}<br />
+              Description: {walk.descriptionS}<br />
+              Distance: {walk.distance} km
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
  )
 }
--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -3,14 +3,14 @@ import Greatwalks from '../components/GreatWalks'
 
 export default function Home() {
   return (
-    <div className='flex max-w-[900px] flex-col justify-center items-center mx-auto'>
-    <div className='lg:px-5 lg:py-10 py-5 lg:text-left text-center items-center'>
-      <h1 className='font-lato lg:mb-5 mb-2 lg:text-left lg:text-4xl text-center text-1xl pr-8'>
+    <div className='flex flex-col justify-center items-center mx-auto px-4 sm:px-6 md:px-8 lg:px-10'>
+    <div className='lg:px-5 lg:py-10 py-5 text-center w-full lg:max-w-4xl'>
+      <h1 className='font-lato lg:mb-5 mb-2 lg:text-left text-center text-xl sm:text-2xl lg:text-4xl pr-8'>
         The great walks of New Zealand, mapped
       </h1>
       <Map />
       <Greatwalks />
     </div>
-    </div>
+  </div>
   )
 }
