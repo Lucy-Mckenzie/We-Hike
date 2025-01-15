9b153193b6ae97b94423aae07b2cde5a91ca4d73
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-15 21:08:37 +1300

    fixing error in deploy

--- client/pages/HutsByRegion.tsx
+++ client/pages/HutsByRegion.tsx
@@ -18,7 +18,7 @@ export default function DisplayRegions() {
       <h1 className='text-xl sm:text-2xl lg:text-4xl text-center mb-5 font-normal'>All Doc huts in New Zealand</h1>
       <MapHuts />
       <div className='flex flex-col items-center max-w-[900px] m-auto pb-6'>
-<!--         <HutAlerts /> -->
+    {/* <HutAlerts />  */}
       <h1 className='text-xl sm:text-2xl lg:text-4xl text-left mb-5 font-light font-lato m-5'>Check out the huts in your Region</h1>
       <ul>
      {filteredRegions.map((region) => {
