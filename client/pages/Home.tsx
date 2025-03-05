843b34681cffaf6df0ebe4f944991a352a4bba13
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-03-06 08:42:00 +1300

    Fixing favicon and formatting.

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -3,9 +3,9 @@ import Grid from '../components/Grid'
 export default function Home() {
   return (
     <div className='relative min-h-screen flex flex-col justify-center items-center'>
-      <div className="absolute top-0 left-0 w-full h-full grid grid-cols-[repeat(auto-fit,minmax(10px,1fr))] grid-rows-[repeat(auto-fit,minmax(10px,1fr))] gap-0 opacity-10">
+      <div className='absolute top-0 left-0 w-full h-full grid grid-cols-[repeat(auto-fit,minmax(10px,1fr))] grid-rows-[repeat(auto-fit,minmax(10px,1fr))] gap-0 opacity-10'>
         {Array.from({ length: 10000 }).map((_, i) => (
-          <div key={i} className="border border-gray-300"></div>
+          <div key={i} className='border border-gray-300'></div>
         ))}
       </div>
       <div className='lg:px-5 lg:py-10 py-5 text-center w-full lg:max-w-7xl'>
